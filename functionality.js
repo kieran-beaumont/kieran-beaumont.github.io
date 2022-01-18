927288f5cdf89614694c9b733e0d36b1fe49038b
kieran-beaumont <kieran.beaumont18@gmail.com>
2022-01-17 19:56:22 -0500

    v2

--- functionality.js
+++ functionality.js
@@ -1,5 +1,7 @@
 function togglePage(id) {
     activePage = document.getElementsByClassName("active");
-    activePage[0].className = "inactive";
-    document.getElementById(id).className = "active";
+    activePage[0].classList.add("inactive");
+    activePage[0].classList.remove("active");
+    document.getElementById(id).classList.add("active");
+    document.getElementById(id).classList.remove("inactive");
 }
